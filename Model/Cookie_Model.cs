54a5930cb1c18de4c8c8dadd0e358da051e58af7
Louis-Hu <58072127+Louis-Hu@users.noreply.github.com>
2019-11-22 18:36:20 +0800

    Add files via upload

---
+++ Model/Cookie_Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsoleApp1.Model
+{
+    /// <summary>
+    ///     Cookie_Model
+    /// </summary>
+    public class Cookie_Model
+    {
+        /// <summary>
+        ///     cookie_id
+        /// </summary>
+        public string cookie_id { get; set; }
+        /// <summary>
+        ///     l1_product
+        /// </summary>
+        public string l1_product { get; set; }
+        /// <summary>
+        ///     l2_product
+        /// </summary>
+        public string l2_product { get; set; }
+        /// <summary>
+        ///     datetime
+        /// </summary>
+        public string datetime { get; set; }
+        /// <summary>
+        ///     startdate
+        /// </summary>
+        public string startdate { get; set; }
+        /// <summary>
+        ///     enddate
+        /// </summary>
+        public string enddate { get; set; }
+        /// <summary>
+        ///     score
+        /// </summary>
+        public string score { get; set; }
+    }
+}
---
+++ Model/DMPtags_Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsoleApp1.Model
+{
+    /// <summary>
+    ///     DMPtags_Modelcs
+    /// </summary>
+    public class DMPtags_Model
+    {
+        /// <summary>
+        ///     cookie_id
+        /// </summary>
+        public string cookie_id { get; set; }
+        /// <summary>
+        ///     dmptags
+        /// </summary>
+        public string dmptags { get; set; }
+    }
+}
